e1b458b4be4becf03234d4dcbf64f1ca1079fabe
Meshack Lukhele <meshacklukhele0513@gmail.com>
2025-11-19 10:20:25 +0000

    These are the app credentials: // Import the functions you need from the

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgvymR-WG4orR2jXbpsPP9sWqlA8xpgSI",
+  authDomain: "ericasite-71cda.firebaseapp.com",
+  projectId: "ericasite-71cda",
+  storageBucket: "ericasite-71cda.appspot.com",
+  messagingSenderId: "419693340494",
+  appId: "1:419693340494:web:41a297b0fd304175f7601f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
