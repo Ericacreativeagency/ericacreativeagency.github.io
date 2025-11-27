eb1462faccbadbfee8875b7c778af7999c5ca79f
Meshack Lukhele <meshacklukhele0513@gmail.com>
2025-11-27 12:09:42 +0000

    🛠️ Final next.config.ts Resolution
    1. Manually Resolve next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-<<<<<<< HEAD
   basePath: '/ericacreativeagency.github.io',
   trailingSlash: true,
-=======
-  basePath: '/Ericacreativeagency',
-  trailingSlash: true,
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  devServer: {
-    allowedDevOrigins: ['*.cloudworkstations.dev'],
-  },
->>>>>>> 7f26fcd9 (Open your next.config.js file and ensure the following properties are se)
   images: {
     unoptimized: true,
     remotePatterns: [
